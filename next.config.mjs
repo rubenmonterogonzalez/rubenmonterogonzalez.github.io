63f5769c72ac22e45224d037d29f39e91f110f30
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2024-09-26 17:12:12 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: 'export', // Enables static export mode
+  trailingSlash: true, // Adds trailing slash to all routes
+  images: {
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  basePath: '', // Optional: Adjust if you're using a subpath in your repository
+  assetPrefix: './', // Use relative paths for assets
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <section className="relative flex h-[100vh] w-full items-center justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
