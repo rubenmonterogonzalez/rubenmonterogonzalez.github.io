0855acf23e2fba9aa1ddd11a97aa44fdf022c131
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2024-10-02 12:24:17 +0200

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,31 +9,31 @@ export const projects = [
     id: "9f4d390d-1291-4523-8875-3422c0d37ba3",
     name: "Datalitiks",
     img: "/images/datalitiks.png",
-    linkToPage: "datalitiks.com",
+    linkToPage: "https://datalitiks.com",
   },
   {
     id: "6a29d826-fbba-4093-8eef-ac3642c79bbf",
     name: "ClearVue",
     img: "/images/clearvue.png",
-    linkToPage: "clearvue.business",
+    linkToPage: "https://clearvue.business",
   },
   {
     id: "4e701cf5-98a2-4f42-95b5-758cc122d08d",
     name: "Northern Gas & Power",
     img: "/images/ngp.png",
-    linkToPage: "ngpltd.co.uk",
+    linkToPage: "https://ngpltd.co.uk",
   },
 
   {
     id: "e6b700d5-1110-4de3-8d4f-102aa947437d",
     name: "Energies France",
     img: "/images/energiesfrance.png",
-    linkToPage: "energiesfrance.fr",
+    linkToPage: "https://energiesfrance.fr",
   },
   {
     id: "5ff76e76-4fc3-4ba4-8652-1866a8e734db",
     name: "Ngp Careers",
     img: "/images/ngp-careers.png",
-    linkToPage: "ngpcareers.com",
+    linkToPage: "https://ngpcareers.com",
   },
 ];
